389500d657f35f6171b05445fc466794e65a6b92
David Chambers <dlchambersjr@gmail.com>
2019-04-27 15:43:23 -0700

    updates labs and demos

--- class-08/demo/server.js
+++ class-08/demo/server.js
@@ -7,11 +7,20 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+const pg = require('pg');
 
 // Application Setup
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT;
+
+//MAC: DATABASE_URL=postgres://localhost:5432/city_explorer
+//WINDOWS: DATABASE_URL=postgres://<user-name>:<password>/@localhost:5432/city_explorer
+
+//Connect to the Database
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.log(err));
 
 // API Routes
 app.get('/location', searchToLatLong);
@@ -29,16 +38,70 @@ function handleError(err, res) {
 
 // Helper Functions
 
+// What we need to do to refactor for SQL Storage
+// 1. We need to check the database to see if the location exists
+//  a. If it exists => get the location from thre database
+//  b. Return the locaiton info to the front-end
+
+// 2. If the location is not in the DB
+//  a. Get the location from the API
+//  b. Run the data through through the constructor
+//  c. Save it to the Database
+//  d. Add the newly added location id to the location object
+//  e. Return the location to the front-end.
+
+
+// function searchToLatLong(request, response) {
+//   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
+
+//   return superagent.get(url)
+//     .then(result => {
+//       response.send(new Location(request.query.data, result.body.results[0]));
+//     })
+//     .catch(error => handleError(error, response));
+// }
+
 function searchToLatLong(request, response) {
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
+  let query = request.query.data;
 
-  return superagent.get(url)
+  // Define the search query
+  let sql = `SELECT * FROM locations WHERE search_query=$1;`;
+  let values = [query];
+
+  console.log('line 71', sql, values);
+
+  // Make the query of the Database
+  client.query(sql, values)
     .then(result => {
-      response.send(new Location(request.query.data, result.body.results[0]));
-    })
-    .catch(error => handleError(error, response));
-}
+      console.log('result from Database', result.rowCount);
+      // Did the DB return any info?
+      if (result.rowCount > 0) {
+        response.send(result.rows[0]);
+      } else {
+        // otherwise go get the data from the API
+        const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
 
+        superagent.get(url)
+          .then(result => {
+            if (!result.body.results.length) { throw 'NO DATA'; }
+            else {
+              let location = new Location(query, result.body.results[0]);
+
+              let newSQL = `INSERT INTO locations (search_query, formatted_address, latitude, longitude) VALUES ($1, $2, $3, $4) RETURNING ID;`;
+              let newValues = Object.values(location);
+
+              client.query(newSQL, newValues)
+                .then(data => {
+                  // attach the returning id to the location object
+                  location.id = data.rows[0].id;
+                  response.send(location);
+                });
+            }
+          })
+          .catch(error => handleError(error, response));
+      }
+    });
+}
 function Location(query, location) {
   this.search_query = query;
   this.formatted_query = location.formatted_address;
@@ -46,17 +109,62 @@ function Location(query, location) {
   this.longitude = location.geometry.location.lng;
 }
 
-function getWeather(request, response) {
-  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+// function getWeather(request, response) {
+//   const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
 
-  return superagent.get(url)
-    .then(weatherResults => {
-      const weatherSummaries = weatherResults.body.daily.data.map(day => {
-        return new Weather(day);
-      });
-      response.send(weatherSummaries);
-    })
-    .catch(error => handleError(error, response));
+//   return superagent.get(url)
+//     .then(weatherResults => {
+//       const weatherSummaries = weatherResults.body.daily.data.map(day => {
+//         return new Weather(day);
+//       });
+//       response.send(weatherSummaries);
+//     })
+//     .catch(error => handleError(error, response));
+// }
+
+function getWeather(request, response) {
+  let query = request.query.data.id;
+  let sql = `SELECT * FROM weathers WHERE location_id=$1;`;
+  let values = [query];
+
+  client.query(sql, values)
+    .then(result => {
+      if (result.rowCount > 0) {
+        console.log('Weather from SQL');
+        response.send(result.rows);
+      } else {
+        const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+
+        return superagent.get(url)
+          .then(weatherResults => {
+            console.log('Weather from API');
+            if (!weatherResults.body.daily.data.length) { throw 'NO DATA'; }
+            else {
+              const weatherSummaries = weatherResults.body.daily.data.map(day => {
+                let summary = new Weather(day);
+                summary.id = query;
+
+                let newSql = `INSERT INTO weathers (forecast, time, location_id) VALUES($1, $2, $3);`;
+                let newValues = Object.values(summary);
+                console.log(newValues);
+                client.query(newSql, newValues);
+
+                return summary;
+
+              });
+              response.send(weatherSummaries);
+            }
+
+          })
+          .catch(error => handleError(error, response));
+      }
+
+
+
+    });
+
+
+
 }
 
 function Weather(day) {
