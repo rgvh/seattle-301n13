4223fb7935df29f51fc10d68b11f0b70e764b25c
David Chambers <dlchambersjr@gmail.com>
2019-04-27 08:23:23 -0700

    adds lab and demo for day-08

---
+++ class-08/demo/server.js
@@ -0,0 +1,87 @@
+'use strict';
+
+// Load Environment Variables from the .env file
+require('dotenv').config();
+
+// Application Dependencies
+const express = require('express');
+const cors = require('cors');
+const superagent = require('superagent');
+
+// Application Setup
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT;
+
+// API Routes
+app.get('/location', searchToLatLong);
+app.get('/weather', getWeather);
+app.get('/events', getEvents);
+
+// Make sure the server is listening for requests
+app.listen(PORT, () => console.log(`City Explorer Backend is up on ${PORT}`));
+
+// ERROR HANDLER
+function handleError(err, res) {
+  console.error(err);
+  if (res) res.status(500).send('Sorry, something went wrong');
+}
+
+// Helper Functions
+
+function searchToLatLong(request, response) {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
+
+  return superagent.get(url)
+    .then(result => {
+      response.send(new Location(request.query.data, result.body.results[0]));
+    })
+    .catch(error => handleError(error, response));
+}
+
+function Location(query, location) {
+  this.search_query = query;
+  this.formatted_query = location.formatted_address;
+  this.latitude = location.geometry.location.lat;
+  this.longitude = location.geometry.location.lng;
+}
+
+function getWeather(request, response) {
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+
+  return superagent.get(url)
+    .then(weatherResults => {
+      const weatherSummaries = weatherResults.body.daily.data.map(day => {
+        return new Weather(day);
+      });
+      response.send(weatherSummaries);
+    })
+    .catch(error => handleError(error, response));
+}
+
+function Weather(day) {
+  this.forecast = day.summary;
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
+}
+
+function getEvents(request, response) {
+  const url = `https://www.eventbriteapi.com/v3/events/search?token=${process.env.EVENTBRITE_API_KEY}&location.address=${request.query.data.formatted_query}`;
+
+  superagent.get(url)
+    .then(result => {
+      const events = result.body.events.map(eventData => {
+        const event = new Event(eventData);
+        return event;
+      });
+
+      response.send(events);
+    })
+    .catch(error => handleError(error, response));
+}
+
+function Event(event) {
+  this.link = event.url;
+  this.name = event.name.text;
+  this.event_date = new Date(event.start.local).toString().slice(0, 15);
+  this.summary = event.summary;
+}
