a004e1fa50cdb4907eda00ee037f2a57257fd4df
David Chambers <dlchambersjr@gmail.com>
2019-05-07 19:55:06 -0700

    updates demo

--- class-11/demos/forms/server.js
+++ class-11/demos/forms/server.js
@@ -5,12 +5,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
 app.post('/contact', (request, response) => {
   console.log(request.body);
-  response.sendFile('./thanks.html', {root: './public'});
+  response.sendFile('./thanks.html', { root: './public' });
 });
 
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
--- class-11/demos/templating/server.js
+++ class-11/demos/templating/server.js
@@ -8,14 +8,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Set the view engine for templating
-
+app.set('view engine', 'ejs');
 
 // Array of groceries for /list route
-let list = ['apples', 'celery', 'butter', 'milk', 'eggs'];
+let list = ['MacBook Banana', 'celery', 'butter', 'milk', 'eggs'];
 
-// Array of quantities for /details route
+// Array of quantities for /quantities route
 let quantities = [
-  { name: 'apples', quantity: 4 },
+  { name: 'MacBook Banana', quantity: 4 },
   { name: 'celery', quantity: 1 },
   { name: 'butter', quantity: 1 },
   { name: 'milk', quantity: 2 },
@@ -24,17 +24,16 @@ let quantities = [
 
 // Routes
 app.get('/', (request, response) => {
-
+  response.render('index');
 })
 
 app.get('/list', (request, response) => {
-
+  response.render('list', { arrayOfItems: list });
 })
 
 app.get('/quantities', (request, response) => {
-
+  response.render('quantities', { groceryObjects: quantities });
 })
-
 
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
